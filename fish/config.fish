344bf502428d1628c212f46735f7371c8906b2d8
peyton-spencer <pas1234567@gmail.com>
2023-08-27 20:40:27 -0600

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+ [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
